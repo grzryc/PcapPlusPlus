dd31a0911c152083c719b1e2913b3e96457c3cce
Grzegorz Rychlik <g.rychlik@cryptomage.io>
2017-10-02 18:02:31 +0200

    icmpv6 support

---
+++ Packet++/header/Icmpv6Layer.h
@@ -0,0 +1,174 @@
+#ifndef PACKETPP_ICMPV6_LAYER
+#define PACKETPP_ICMPV6_LAYER
+
+#include <Layer.h>
+#include <IPv6Layer.h>
+#include <IcmpLayer.h>
+#ifdef _MSC_VER
+#include <Winsock2.h>
+#else
+#include <sys/time.h>
+#endif
+#include <vector>
+
+
+/// @file
+
+/**
+ * \namespace pcpp
+ * \brief The main namespace for the PcapPlusPlus lib
+ */
+namespace pcpp
+{
+
+	/**
+     * @TODO review
+	 * An enum of all supported ICMPv6 message types
+	 */
+	enum Icmpv6MessageType
+	{
+        //@TODO docs
+		/** ICMPV6 destination unreachable message */
+		ICMPV6_DEST_UNREACHABLE     = 1,
+		/** ICMPV6 packet too big message */
+		ICMPV6_PACKET_TOO_BIG       = 2,
+		/** ICMPV6 time exceeded message */
+		ICMPV6_TIME_EXCEEDED        = 3,
+		/** ICMPV6 parameter problem message */
+		ICMPV6_PARAM_PROBLEM        = 4,
+        /** ICMPv6 echo request message */
+        ICMPV6_ECHO_REQUEST         = 128,
+        ICMPV6_ECHO_REPLY           = 129,
+        ICMPV6_MULTICAST_LIS_QUERY  = 130,
+        ICMPV6_MULTICAST_LIS_REPORT = 131,
+        ICMPV6_MULTICAST_LIS_DONE = 132,
+        ICMPV6_ROUTER_SOL = 133,
+        ICMPV6_ROUTER_ADV = 134,
+        ICMPV6_NEIGHBOR_SOL = 135,
+        ICMPV6_NEIGHBOR_ADV = 136,
+        ICMPV6_REDIRECT = 137,
+        ICMPV6_ROUTER_RENUMBER = 138,
+        ICMPV6_NODE_INFO_QUERY = 139,
+        ICMPV6_NODE_INFO_RESPONSE = 140,
+        ICMPV6_INV_NEIGHBOR_DISCOVERY_SOL = 141,
+        ICMPV6_INV_NEIGHBOR_DISCOVERY_ADV = 142,
+        ICMPV6_MUTICAST_LIS_REPORT_V2 = 143,
+        ICMPV6_HOME_AGENT_ADDR_DISCOV_REQUEST = 144,
+        ICMPV6_HOME_AGENT_ADDR_DISCOV_REPLY = 145,
+        ICMPV6_MOBILE_PREFIX_SOL = 146,
+        ICMPV6_MOBILE_PREFIX_ADV = 147,
+        ICMPV6_CERT_PATH_SOL = 148,
+        ICMPV6_CERT_PATH_ADV = 149,
+        ICMPV6_MULTICAST_ROUTER_ADV = 151,
+        ICMPV6_MULTICAST_ROUTER_SOL = 152,
+        ICMPV6_MULTICAST_ROUTER_TERM = 153,
+        ICMPV6_FIMPV6 = 154,
+        ICMPV6_RPL_CONTROL = 155,
+        ICMPV6_ILNPV6_LOCATOR_UPDATE = 156,
+        ICMPV6_DUPLICATE_ADDR_REQUEST = 157,
+        ICMPV6_DUPLICATE_ADDR_CONFIRM = 158,
+        ICMPV6_MPL_CONTROL = 159,
+        ICMPV6_UNSUPPORTED = 255
+	};
+
+	/**
+     * @TODO review
+	 * An enum for all possible codes for a destination unreachable message type
+	 * Documentation is taken from Wikipedia: https://en.wikipedia.org/wiki/Internet_Control_Message_Protocol_version_6
+	 */
+	enum Icmpv6DestUnreachableCodes
+	{
+		/** @TODO docs*/
+        Icmpv6NoRouteToDestination = 0,
+		Icmpv6CommunicationProhibited = 1,
+		Icmpv6BeyondScopeOfSourceAddress = 2,
+		Icmpv6AddressUnreachable = 3,
+        Icmpv6PortUnreachable = 4,
+        Icmpv6SourceAddrFailedPolicy = 5,
+        Icmpv6RejectRouteToDest = 6,
+        Icmpv6ErrorInSourceRoutingHeader = 7
+	};
+
+    
+	/**
+	 * @class Icmpv6Layer
+	 * Represents an ICMPv6 protocol layer (for IPv6 only)
+	 */
+	class Icmpv6Layer : public Layer
+	{
+	private:
+//		icmp_echo_request m_EchoData;
+//		icmp_router_advertisement m_RouterAdvData;
+
+		bool cleanIcmpv6Layer();
+
+//		bool setEchoData(IcmpMessageType echoType, uint16_t id, uint16_t sequence, uint64_t timestamp, const uint8_t* data, size_t dataLen);
+
+//		bool setIpAndL4Layers(IPv4Layer* ipLayer, Layer* l4Layer);
+
+	public:
+		/**
+		 * A constructor that creates the layer from an existing packet raw data
+		 * @param[in] data A pointer to the raw data (will be casted to @ref arphdr)
+		 * @param[in] dataLen Size of the data in bytes
+		 * @param[in] prevLayer A pointer to the previous layer
+		 * @param[in] packet A pointer to the Packet instance where layer will be stored in
+		 */
+		Icmpv6Layer(uint8_t* data, size_t dataLen, Layer* prevLayer, Packet* packet) : Layer(data, dataLen, prevLayer, packet) { m_Protocol = ICMP; }
+
+		/**
+		 * An empty constructor that creates a new layer with an empty ICMP header without setting the ICMP type or ICMP data.
+		 * Call the set*Data() methods to set ICMP type and data
+		 */
+		Icmpv6Layer();
+
+		virtual ~Icmpv6Layer() {}
+
+		/**
+		 * Get a pointer to the basic ICMP header. Notice this points directly to the data, so every change will change the actual packet data
+		 * @return A pointer to the @ref icmphdr
+		 */
+		inline icmphdr* getIcmpv6Header() { return (icmphdr*)m_Data; };
+
+		/**
+		 * @return The ICMP message type
+		 */
+		Icmpv6MessageType getMessageType();
+
+		/**
+		 * @param[in] type Type to check
+		 * @return True if the layer if of the given type, false otherwise
+		 */
+		bool isMessageOfType(Icmpv6MessageType type);
+
+
+
+		// implement abstract methods
+
+		/**
+		 * ICMP messages of types: ICMP_DEST_UNREACHABLE, ICMP_SOURCE_QUENCH, ICMP_TIME_EXCEEDED, ICMP_REDIRECT, ICMP_PARAM_PROBLEM
+		 * have data that contains IPv4 header and some L4 header (TCP/UDP/ICMP). This method parses these headers as separate
+		 * layers on top of the ICMP layer
+		 */
+		void parseNextLayer();
+
+		/**
+		 * @return The ICMP header length. This length varies according to the ICMP message type. This length doesn't include
+		 * IPv4 and L4 headers in case ICMP message type are: ICMP_DEST_UNREACHABLE, ICMP_SOURCE_QUENCH, ICMP_TIME_EXCEEDED,
+		 * ICMP_REDIRECT, ICMP_PARAM_PROBLEM
+		 */
+		size_t getHeaderLen();
+
+		/**
+		 * Calculate ICMP checksum field
+		 */
+		void computeCalculateFields();
+
+		std::string toString();
+
+		OsiModelLayer getOsiModelLayer() { return OsiModelNetworkLayer; }
+	};
+
+} // namespace pcpp
+
+#endif /* PACKETPP_ICMPV6_LAYER */
---
+++ Packet++/src/Icmpv6Layer.cpp
@@ -0,0 +1,216 @@
+#define LOG_MODULE PacketLogModuleIcmpv6Layer
+
+#include <Icmpv6Layer.h>
+#include <PayloadLayer.h>
+#include <Packet.h>
+#include <IpUtils.h>
+#include <Logger.h>
+#include <sstream>
+#include <string.h>
+#if defined(WIN32) || defined(WINx64) //for using ntohl, ntohs, etc.
+#include <winsock2.h>
+#elif LINUX
+#include <in.h> //for using ntohl, ntohs, etc.
+#elif MAC_OS_X
+#include <arpa/inet.h> //for using ntohl, ntohs, etc.
+#endif
+
+namespace pcpp
+{
+
+Icmpv6Layer::Icmpv6Layer() : Layer()
+{
+	m_DataLen = sizeof(icmphdr);
+	m_Data = new uint8_t[m_DataLen];
+	memset(m_Data, 0, m_DataLen);
+	m_Protocol = ICMP;
+}
+
+Icmpv6MessageType Icmpv6Layer::getMessageType()
+{
+	uint8_t type = getIcmpv6Header()->type;
+	if (type > 159)
+		return ICMPV6_UNSUPPORTED;
+
+	return (Icmpv6MessageType)type;
+}
+
+bool Icmpv6Layer::isMessageOfType(Icmpv6MessageType type)
+{
+	return (getMessageType() == type);
+}
+
+bool Icmpv6Layer::cleanIcmpv6Layer()
+{
+	// remove all layers after
+
+	if (m_Packet != NULL)
+	{
+		Layer* layerToRemove = this->getNextLayer();
+		while (layerToRemove != NULL)
+		{
+			Layer* temp = layerToRemove->getNextLayer();
+			if (!m_Packet->removeLayer(layerToRemove))
+				return false;
+			layerToRemove = temp;
+		}
+	}
+
+
+	// shorten layer to size of icmphdr
+
+	size_t headerLen = this->getHeaderLen();
+	if (headerLen > sizeof(icmphdr))
+	{
+		if (!this->shortenLayer(sizeof(icmphdr), headerLen - sizeof(icmphdr)))
+			return false;
+	}
+
+	return true;
+}
+
+
+void Icmpv6Layer::parseNextLayer()
+{
+	Icmpv6MessageType type = getMessageType();
+	size_t headerLen = 0;
+
+	switch (type)
+	{
+	case ICMPV6_DEST_UNREACHABLE:
+// @TODO make and review
+//		headerLen = getHeaderLen();
+//		if (m_DataLen - headerLen >= sizeof(iphdr))
+//			m_NextLayer = new IPv4Layer(m_Data + headerLen, m_DataLen - headerLen, this, m_Packet);
+//		return;
+	default:
+		headerLen = getHeaderLen();
+		if (m_DataLen > headerLen)
+			m_NextLayer = new PayloadLayer(m_Data + headerLen, m_DataLen - headerLen, this, m_Packet);
+		return;
+	}
+}
+
+size_t Icmpv6Layer::getHeaderLen()
+{
+	Icmpv6MessageType type = getMessageType();
+	switch (type)
+	{
+	case ICMPV6_ECHO_REQUEST:
+//        @TODO
+	default:
+		return sizeof(icmphdr);
+	}
+}
+
+void Icmpv6Layer::computeCalculateFields()
+{
+	// calculate checksum
+	getIcmpv6Header()->checksum = 0;
+
+	size_t icmpLen = 0;
+	Layer* curLayer = this;
+	while (curLayer != NULL)
+	{
+		icmpLen += curLayer->getHeaderLen();
+		curLayer = curLayer->getNextLayer();
+	}
+
+	ScalarBuffer<uint16_t> buffer;
+	buffer.buffer = (uint16_t*)getIcmpv6Header();
+	buffer.len = icmpLen;
+	size_t checksum = compute_checksum(&buffer, 1);
+
+	getIcmpv6Header()->checksum = htons(checksum);
+}
+
+std::string Icmpv6Layer::toString()
+{
+	std::string messageTypeAsString;
+	Icmpv6MessageType type = getMessageType();
+	switch (type)
+	{
+	case ICMPV6_DEST_UNREACHABLE:
+		messageTypeAsString = "Destination unreachable";
+		break;
+    case ICMPV6_PACKET_TOO_BIG:
+        messageTypeAsString = "Packet too big";
+        break;
+    case ICMPV6_TIME_EXCEEDED:
+        messageTypeAsString = "Time exceeded";
+        break;
+    case ICMPV6_PARAM_PROBLEM:
+        messageTypeAsString = "Parameter problem";
+        break;
+    case ICMPV6_ECHO_REQUEST:
+        messageTypeAsString = "Echo (ping) request";
+        break;
+    case ICMPV6_ECHO_REPLY:
+        messageTypeAsString = "Echo (ping) reply";
+        break;
+    case ICMPV6_MULTICAST_LIS_QUERY:
+        messageTypeAsString = "Multicast listener query";
+        break;
+    case ICMPV6_MULTICAST_LIS_REPORT:
+        messageTypeAsString = "Multicast listener report";
+        break;
+    case ICMPV6_MULTICAST_LIS_DONE:
+        messageTypeAsString = "Multicast listener done";
+        break;
+    case ICMPV6_ROUTER_SOL:
+        messageTypeAsString = "Router solicitation";
+        break;
+    case ICMPV6_ROUTER_ADV:
+        messageTypeAsString = "Router advertisement";
+        break;
+    case ICMPV6_NEIGHBOR_SOL:
+        messageTypeAsString = "Neighbor solicitation";
+        break;
+    case ICMPV6_NEIGHBOR_ADV:
+        messageTypeAsString = "Neighbor advertisement";
+        break;
+    case ICMPV6_REDIRECT:
+        messageTypeAsString = "Redirect";
+        break;
+    case ICMPV6_ROUTER_RENUMBER:
+        messageTypeAsString = "Router renumbering";
+        break;
+    case ICMPV6_NODE_INFO_QUERY:
+        messageTypeAsString = "ICMP node information query";
+        break;
+    case ICMPV6_NODE_INFO_RESPONSE:
+        messageTypeAsString = "ICMP node information response";
+        break;
+    case ICMPV6_INV_NEIGHBOR_DISCOVERY_SOL:
+        messageTypeAsString = "Inverse neighbor discovery solicitation message";
+        break;
+    case ICMPV6_INV_NEIGHBOR_DISCOVERY_ADV:
+        messageTypeAsString = "Inverse neighbor discovery advertisement message";
+        break;
+    case ICMPV6_MUTICAST_LIS_REPORT_V2:
+        messageTypeAsString = "Version 2 multicast listener report";
+        break;
+    case ICMPV6_HOME_AGENT_ADDR_DISCOV_REQUEST:
+        messageTypeAsString = "Home agent address discovery request";
+        break;
+    case ICMPV6_HOME_AGENT_ADDR_DISCOV_REPLY:
+        messageTypeAsString = "Home agent address discovery reply";
+        break;
+    case ICMPV6_MOBILE_PREFIX_SOL:
+        messageTypeAsString = "Mobile prefix solicitation";
+        break;
+        
+        
+    // @TODO i'm a stub, complete me
+	default:
+		messageTypeAsString = "Unknown";
+		break;
+	}
+
+	std::ostringstream typeStream;
+	typeStream << (int)getIcmpv6Header()->type;
+
+	return "ICMPv6 Layer, " + messageTypeAsString + " (type: " + typeStream.str() + ")";
+}
+
+} // namespace pcpp
